81bef8f780a989f74b1b59ddea0ff6e07a0bfd98
Sebastien Alaiwan <sebastien.alaiwan@gmail.com>
2018-12-16 12:54:56 +0100

    Introduce struct

--- example/snesc.c
+++ example/snesc.c
@@ -37,8 +37,13 @@ static const char* ST_BLANK = "        ";
 int dx = 2, dy = 1, px = 80;
 unsigned int x = 94, y = 109;
 unsigned int i, j, b = 0, c, obx, oby, bx = 5, by = 11, py = 0;
-int xdir[4] = { -2, -1, 1, 2 };
-int ydir[4] = { -1, -2, -2, -1 };
+
+typedef struct
+{
+  int x, y;
+} Vec2;
+
+Vec2 dir[4] = {{-2, -1}, {-1, -2}, {1, -2}, {2, -1}};
 unsigned int blockcount = 0;
 unsigned long long score = 0, hiscore = 50000;
 unsigned int level2 = 1;
@@ -172,8 +177,8 @@ void run_frame()
       if((x >= px) && (x <= px + 27))
       {
         int a = (x - px) / 7;
-        dx = xdir[a];
-        dy = ydir[a];
+        dx = dir[a].x;
+        dy = dir[a].y;
       }
     }
     else if(y > 224)

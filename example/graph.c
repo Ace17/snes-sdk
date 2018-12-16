9c4623594a2501c75244f5539d1d9c2941c713c0
Sebastien Alaiwan <sebastien.alaiwan@gmail.com>
2018-12-16 12:06:18 +0100

    Introduce 'poke'

--- example/graph.c
+++ example/graph.c
@@ -3,21 +3,27 @@
 
 void snesc_vblank(void);
 
+inline
+void poke(unsigned short address, unsigned char value)
+{
+  *(unsigned char*)address = value;
+}
+
 void enablescreen(void)
 {
-  *(unsigned char*)0x2105 = 9; /* BG mode 1 */
-  *(unsigned char*)0x2109 = 4; /* BG3 map location */
-  *(unsigned char*)0x210b = 1; /* BG1/2 tiles location */
-  *(unsigned char*)0x210c = 2; /* BG3/4 tiles location */
-  *(unsigned char*)0x212c = 0x15; /* enable BG1, BG3, OBJ display */
-  *(unsigned char*)0x2100 = 0x80; /* force blank */
-  snesc_vblank();	/* flush pending DMA jobs before turning on screen */
-  *(unsigned char*)0x2100 = 0xf; /* turn on screen, full brightness */
+  poke(0x2105, 9); // BG mode 1
+  poke(0x2109, 4); // BG3 map location
+  poke(0x210b, 1); // BG1/2 tiles location
+  poke(0x210c, 2); // BG3/4 tiles location
+  poke(0x212c, 0x15); // enable BG1, BG3, OBJ display
+  poke(0x2100, 0x80); // force blank
+  snesc_vblank();	// flush pending DMA jobs before turning on screen
+  poke(0x2100, 0xf); // turn on screen, full brightness
 }
 
 void screenmode(unsigned char m)
 {
-  *(unsigned char*)0x2105 = m;
+  poke(0x2105, m);
 }
 
 void waitforvsync(void)
@@ -83,7 +89,7 @@ void setmap(unsigned int b, unsigned char *p1)
   tr->src.c.type = 0;
   tr->dest = b << 10;
   tr->size = 0x800;
-  
+
   /* signal the NMI to copy data to VRAM */
   snesc_do_copy++;
 }
